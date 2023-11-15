f71f184da2756cab4ad4e3ac9b1a0d5634458a1c
Keerthi-Reddy-Baripireddy <144705049+Keerthi-Reddy-Baripireddy@users.noreply.github.com>
2023-11-15 12:55:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function toCase(text) {
-  // write your code here
+    if (text.length === 0) {
+        return '-';
+    }
+
+    const lowercaseText = text.toLowerCase();
+    const uppercaseText = text.toUpperCase();
+    const result = lowercaseText + '-' + uppercaseText;
+
+    return result;
 }
 
 // DO not change the code below
-
 const text = prompt("Enter text:");
 alert(toCase(text));
